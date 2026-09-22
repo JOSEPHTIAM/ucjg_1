123ee1b5a0cbf42ac0cc9a579cbcfba35f07301a
JOSEPH TIAM <115112733+JOSEPHTIAM@users.noreply.github.com>
2026-09-22 20:20:47 +0100

    8em commit : Refaire la migration du champ COMMENTAIRE dans la vidéo.

--- database/migrations/2026_09_22_190315_add_commentaire_to_video_table.php
+++ database/migrations/2026_09_22_190315_add_commentaire_to_video_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('videos', function (Blueprint $table) {
+        Schema::table('video', function (Blueprint $table) {
             $table->text('commentaire')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('videos', function (Blueprint $table) {
+        Schema::table('video', function (Blueprint $table) {
             $table->dropColumn('commentaire');
         });
     }
